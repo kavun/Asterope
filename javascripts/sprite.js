234ab22f2bd9dc49056b4f1339f57a2c4691a1f4
Ralph Sharp <rshar224@gmail.com>
2013-05-07 18:53:19 -0400

    starting work done
    Work already done prior to creating a Git Repository

---
+++ javascripts/sprite.js
@@ -0,0 +1,68 @@
+(function() {
+  function LoaderProxy() {
+    return {
+      draw: $.noop,
+      fill: $.noop,
+      frame: $.noop,
+      update: $.noop,
+      width: null,
+      height: null
+    };
+  }
+  
+  function Sprite(image, sourceX, sourceY, width, height) {
+    sourceX = sourceX || 0;
+    sourceY = sourceY || 0;
+    width = width || image.width;
+    height = height || image.height;
+    
+    return {
+      draw: function(canvas, x, y, angle) {
+        angle = angle || 0;
+
+        canvas.translate(x, y);
+        canvas.rotate(angle);
+        canvas.drawImage(image, -width / 2, -height / 2, width, height);
+        canvas.rotate(-angle);
+        canvas.translate(-x, -y);
+      },
+      
+      fill: function(canvas, x, y, width, height, repeat) {
+        repeat = repeat || "repeat";
+        var pattern = canvas.createPattern(image, repeat);
+        canvas.fillColor(pattern);
+        canvas.fillRect(x, y, width, height);
+      },
+      
+      width: width,
+      height: height
+    };
+  };
+  
+  Sprite.load = function(url, loadedCallback) {
+    var img = new Image();
+    var proxy = LoaderProxy();
+    
+    img.onload = function() {
+      var tile = Sprite(this);
+      
+      $.extend(proxy, tile);
+      
+      if(loadedCallback) {
+        loadedCallback(proxy);
+      }
+    };
+    
+    img.src = url;
+    
+    return proxy;
+  };
+ 
+  var spriteImagePath = "images/";
+
+  window.Sprite = function(name, callback) {
+    return Sprite.load(spriteImagePath + name + ".png", callback);
+  };
+  window.Sprite.EMPTY = LoaderProxy();
+  window.Sprite.load = Sprite.load;
+}());
